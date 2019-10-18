9235d05dc667279a3d5d2017b7c110b6553378cc
Jakub Veverka <veverka.kuba@gmail.com>
2019-10-18 11:57:39 +0200

    test: add Calendar and test listing events

--- simple-calendar-events/index.ts
+++ simple-calendar-events/index.ts
@@ -1,20 +1,40 @@
+
+const getThisMonthRow = (): any[] => {
+  let sheet = SpreadsheetApp.getActiveSheet();
+  let thisRow = sheet.getDataRange().getValues()[1]
+  return thisRow
+}
+
+
 class MonthWork {
   dayShifts: number[];
   nightShifts: number[];
   morningShifts: number[];
   afternoonShifts: number[];
   sheet: GoogleAppsScript.Spreadsheet.Sheet;
+  currentRow: any[];
 
 
+  constructor(thisMonthRange: any[]) {
+    this.dayShifts = thisMonthRange[2]
+    this.nightShifts = thisMonthRange[3]
+    this.morningShifts = thisMonthRange[4]
+    this.afternoonShifts = thisMonthRange[5]
 
-  constructor() {
-    this.sheet = SpreadsheetApp.getActiveSheet();
-    let data = this.sheet.getDataRange().getValues();
-    let dayShifts: string = data[1][2];
-    Logger.log('Denni: ' + dayShifts.split(",")[0] + 'Dalsi: ' + dayShifts.split(",")[1])
+    Logger.log('Denni: ' + this.dayShifts)
+    Logger.log('Nocni: ' + this.nightShifts)
+    Logger.log('Ranni: ' + this.morningShifts)
+    Logger.log('Odpoledni: ' + this.afternoonShifts)
   }
 }
 
+const createEvent = () => {
+  Logger.log('CalendarEvents: ' + Calendar.Events.list('beesr16iv7pam5gval5rh0cm2c@group.calendar.google.com') )
+  Calendar.Events.list('beesr16iv7pam5gval5rh0cm2c@group.calendar.google.com')
+}
+
 function main() {
-  let m = new MonthWork()
+  createEvent()
+  // const thisMonth = getThisMonthRow()
+  // new MonthWork(thisMonth)
 }
